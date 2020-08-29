a32e64e432bc466c51abb984fa96f2bbffd376c8
bolcof <sanctum.crusade.ex@gmail.com>
2020-08-29 23:09:26 +0900

    ?

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public int a;
+    public GameObject Ground;
+
+    void Update()
+    {
+        if (Input.GetKey("up")){
+            Ground.transform.Rotate(new Vector3(1.0f, 0.0f, 0.0f));
+        }
+        if (Input.GetKey("down"))
+        {
+            Ground.transform.Rotate(new Vector3(-1.0f, 0.0f, 0.0f));
+        }
+        if (Input.GetKey("right"))
+        {
+            Ground.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f));
+        }
+        if (Input.GetKey("left"))
+        {
+            Ground.transform.Rotate(new Vector3(0.0f, 0.0f, -1.0f));
+            {
+    }
+}

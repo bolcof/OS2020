66e9702e3d4ab38b512311b47ee8cc72a9e2eeac
bolcof <sanctum.crusade.ex@gmail.com>
2020-08-29 23:39:55 +0900

    動きができた

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class MoveScript : MonoBehaviour
 {
-    public int a;
     public GameObject Ground;
 
     void Update()
     {
         if (Input.GetKey("up")){
-            Ground.transform.Rotate(new Vector3(1.0f, 0.0f, 0.0f));
+            Ground.transform.Rotate(new Vector3(0.4f, 0.0f, 0.0f), Space.World);
         }
         if (Input.GetKey("down"))
         {
-            Ground.transform.Rotate(new Vector3(-1.0f, 0.0f, 0.0f));
+            Ground.transform.Rotate(new Vector3(-0.4f, 0.0f, 0.0f), Space.World);
         }
         if (Input.GetKey("right"))
         {
-            Ground.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f));
+            Ground.transform.Rotate(new Vector3(0.0f, 0.8f, 0.0f), Space.World);
         }
         if (Input.GetKey("left"))
         {
-            Ground.transform.Rotate(new Vector3(0.0f, 0.0f, -1.0f));
-            {
+            Ground.transform.Rotate(new Vector3(0.0f, -0.8f, 0.0f), Space.World);
+        }
     }
 }

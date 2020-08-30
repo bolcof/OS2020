2bf802bed9778ea7c4834cdcb13a6c6a819e4c80
bolcof <sanctum.crusade.ex@gmail.com>
2020-08-30 13:44:52 +0900

    movie script

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class MoveScript : MonoBehaviour
 {
     public GameObject Ground;
+    public bool isActive;
 
     void Update()
     {
-        if (Input.GetKey("up")){
-            Ground.transform.Rotate(new Vector3(0.4f, 0.0f, 0.0f), Space.World);
-        }
-        if (Input.GetKey("down"))
+        if (isActive)
         {
-            Ground.transform.Rotate(new Vector3(-0.4f, 0.0f, 0.0f), Space.World);
-        }
-        if (Input.GetKey("right"))
-        {
-            Ground.transform.Rotate(new Vector3(0.0f, 0.8f, 0.0f), Space.World);
-        }
-        if (Input.GetKey("left"))
-        {
-            Ground.transform.Rotate(new Vector3(0.0f, -0.8f, 0.0f), Space.World);
+            if (Input.GetKey("up") || Input.GetKey(KeyCode.W))
+            {
+                Ground.transform.Rotate(new Vector3(0.4f, 0.0f, 0.0f), Space.World);
+            }
+            if (Input.GetKey("down") || Input.GetKey(KeyCode.S))
+            {
+                Ground.transform.Rotate(new Vector3(-0.4f, 0.0f, 0.0f), Space.World);
+            }
+            if (Input.GetKey("right") || Input.GetKey(KeyCode.D))
+            {
+                Ground.transform.Rotate(new Vector3(0.0f, -0.8f, 0.0f), Space.World);
+            }
+            if (Input.GetKey("left") || Input.GetKey(KeyCode.A))
+            {
+                Ground.transform.Rotate(new Vector3(0.0f, 0.8f, 0.0f), Space.World);
+            }
         }
     }
 }
---
+++ Assets/Scripts/MovieUIBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MovieUIBehaviour : MonoBehaviour
+{
+    public float triggerDistance, ExitDistance;
+    public float MaxDistance, MuteDistance;
+
+    public bool isPlaying;
+
+    public GameObject PlayerObj;
+
+    void Update()
+    {
+        float dist = Vector3.Distance(this.transform.position, PlayerObj.transform.position);
+
+        if (!isPlaying)
+        {
+            if (dist < triggerDistance)
+            {
+                isPlaying = true;
+                this.gameObject.GetComponent<VideoPlayer>().time = 0.0f;
+                this.gameObject.GetComponent<VideoPlayer>().Play();
+            }
+        }
+        else
+        {
+            if (dist > ExitDistance)
+            {
+                isPlaying = false;
+                this.gameObject.GetComponent<VideoPlayer>().Stop();
+            }
+        }
+    }
+}

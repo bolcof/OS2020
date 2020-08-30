c2bf5d83dcfbf9b91a16c6a23d06e0876d617cbd
bolcof <sanctum.crusade.ex@gmail.com>
2020-08-30 14:48:18 +0900

    Runnerができた

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    public GameObject origin;
+    private float angleX = 0.2f;
+    public float speed;
+
+    public float MaxDistance, MuteDistance;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        angleX += speed;
+        if (angleX >= 179.9f) { angleX = 179.9f; speed *= -1; }
+        else if (angleX <= 0.1f) { angleX = 0.1f; speed *= -1; }
+        origin.transform.localRotation = Quaternion.Euler(new Vector3(angleX, 0.0f, 0.0f));
+    }
+}

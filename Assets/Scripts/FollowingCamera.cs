456296d8bef14996897d80f034812e3dc7caaed2
bolcof <sanctum.crusade.ex@gmail.com>
2020-09-07 04:45:48 +0900

    球面に沿ったモデル＋カメラスクロール

--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour
 {
-    public GameObject PlayerObj;
-    // Update is called once per frame
+    private float scroll;
+    public float scrollNow, max;
+    private Vector3 startPos;
+    public Vector3 endPos;
+    private Vector3 startRotate;
+    public Vector3 endRotate;
+    private float startFoV;
+    public float endFoV;
+
+    private void Start()
+    {
+        startPos = this.transform.localPosition;
+        startRotate = this.transform.localRotation.eulerAngles;
+        startFoV = Camera.main.fieldOfView;
+    }
+    
     void Update()
     {
-        this.transform.rotation = PlayerObj.transform.rotation;
+        scroll = Input.GetAxis("Mouse ScrollWheel");
+        scrollNow += scroll;
+        if (scrollNow <= 0.0f) { scrollNow = 0.0f; }
+        if (scrollNow >= max) { scrollNow = max; }
+
+        Vector3 difPos = endPos - startPos;
+        Camera.main.transform.localPosition = startPos + difPos * (scrollNow / max);
+
+        Vector3 difRotate = endRotate - startRotate;
+        Camera.main.transform.localRotation = Quaternion.Euler(startRotate + difRotate * (scrollNow / max));
+
+        float difFoV = endFoV - startFoV;
+        Camera.main.fieldOfView = startFoV + difFoV * (scrollNow / max);
     }
 }
